cfb097f6dc330eb6eeb5db9de377a01a82a71c0f
ushasis02 <93474220+ushasis02@users.noreply.github.com>
2022-03-27 18:02:37 +0530

    Full project

---
+++ studentManagement/src/main/java/com/greatLearning/studentManagement/StudentManagementApplication.java
@@ -0,0 +1,21 @@
+package com.greatLearning.studentManagement;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("spring boot application started");
+		
+	}
+
+}
